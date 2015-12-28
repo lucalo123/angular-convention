1bd44185305a6acc5dc1509aaa43d86f57b4ad96
lucalo123 <pallin1@hotmail.com>
2015-12-28 16:48:39 +0100

    server fix

--- src/server/serve.js
+++ src/server/serve.js
@@ -8,6 +8,7 @@ var env = process.env.NODE_ENV;
 switch (env) {
     case 'build':
         console.log('In build-environment.');
+        app.use(express.static('./'));
         app.use(express.static('./build/'));
         app.use('/*', express.static('./build/index.html'));
         break;
